4c99bb8258442132d1a07229cc2a83d2754a3248
Harrison Heeb <harrisonheeb@Harrisons-MacBook-Pro.local>
2019-10-20 14:41:53 -0500

    fix bug

--- battleship/gui_classes.py
+++ battleship/gui_classes.py
@@ -22,15 +22,15 @@ class State:
         self.iterations = 0
 
     def update(self, guess, switch = True):
-    """
-    Updates the state based by adding the guess and "flipping" the players.
-    :param guess: a coordinate
-    :return: void
-    """
-    self.player1.add_guess(guess)
-    if(switch):
-        self.player2, self.player1 = self.player1, self.player2
-    self.iterations += 1
+        """
+        Updates the state based by adding the guess and "flipping" the players.
+        :param guess: a coordinate
+        :return: void
+        """
+        self.player1.add_guess(guess)
+        if(switch):
+            self.player2, self.player1 = self.player1, self.player2
+        self.iterations += 1
 
     def is_game_over(self):
         """
